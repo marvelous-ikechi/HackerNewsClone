bf29677a4acfda7379e20d18c05dec7c1203c3db
marvelous-ikechi <marvelike7@gmail.com>
2024-09-09 20:31:33 +0100

    Login and signup screens completed

--- src/auth/screens/Login.tsx
+++ src/auth/screens/Login.tsx
@@ -1,8 +1,35 @@
 import React, {FunctionComponent} from 'react';
-import {View} from 'react-native';
+import {Text, TextInput, TouchableOpacity, View} from 'react-native';
+import ScreenContainer from '../../components/ScreenContainer';
+import tw from '../../lib/tailwind';
+import SubmitBtn from '../../components/Button/SubmitBtn';
+import {AuthStackParamList} from '../../types/navigationTypes';
+import {NativeStackScreenProps} from 'react-native-screens/lib/typescript/native-stack/types';
 
-const Login: FunctionComponent = () => {
-  return <View></View>;
+type Props = NativeStackScreenProps<AuthStackParamList, 'Login'>;
+const Login: FunctionComponent<Props> = ({navigation}) => {
+  return (
+    <ScreenContainer>
+      <View style={tw`flex-1 w-full justify-center items-center`}>
+        <TextInput
+          placeholderTextColor={'white'}
+          placeholder="Email"
+          style={tw`border-[0.5px] px-3 rounded-md w-80 border-white`}
+        />
+        <TextInput
+          placeholderTextColor={'white'}
+          placeholder="Password"
+          style={tw`border-[0.5px] px-3 mt-4 rounded-md w-80 border-white`}
+        />
+        <SubmitBtn title="Login" onPress={() => {}} />
+        <TouchableOpacity onPress={() => navigation.navigate('Signup')}>
+          <Text style={tw`text-white text-right mt-4`}>
+            Don't have an account? Signup
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </ScreenContainer>
+  );
 };
 
 export default Login;
--- src/auth/screens/Signup.tsx
+++ src/auth/screens/Signup.tsx
@@ -1,8 +1,45 @@
 import React, {FunctionComponent} from 'react';
-import {View} from 'react-native';
+import {Text, TextInput, TouchableOpacity, View} from 'react-native';
+import ScreenContainer from '../../components/ScreenContainer';
+import tw from '../../lib/tailwind';
+import SubmitBtn from '../../components/Button/SubmitBtn';
+import {NativeStackScreenProps} from 'react-native-screens/lib/typescript/native-stack/types';
+import {AuthStackParamList} from '../../types/navigationTypes';
 
-const Signup: FunctionComponent = () => {
-  return <View></View>;
+type Props = NativeStackScreenProps<AuthStackParamList, 'Signup'>;
+const Signup: FunctionComponent<Props> = ({navigation}) => {
+  return (
+    <ScreenContainer>
+      <View style={tw`flex-1 w-full justify-center items-center`}>
+        <TextInput
+          placeholderTextColor={'white'}
+          placeholder="First Name"
+          style={tw`border-[0.5px] px-3 rounded-md w-80 border-white`}
+        />
+        <TextInput
+          placeholderTextColor={'white'}
+          placeholder="Last Name"
+          style={tw`border-[0.5px] mt-4 px-3 rounded-md w-80 border-white`}
+        />
+        <TextInput
+          placeholderTextColor={'white'}
+          placeholder="Email"
+          style={tw`border-[0.5px] mt-4 px-3 rounded-md w-80 border-white`}
+        />
+        <TextInput
+          placeholderTextColor={'white'}
+          placeholder="Password"
+          style={tw`border-[0.5px] px-3 mt-4 rounded-md w-80 border-white`}
+        />
+        <SubmitBtn title="Signup" onPress={() => {}} />
+        <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+          <Text style={tw`text-white text-right mt-4`}>
+            Already have an account? Login
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </ScreenContainer>
+  );
 };
 
 export default Signup;
