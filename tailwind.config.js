4f0fe66c30a649a1967a3e2cab31189850a8887d
marvelous-ikechi <marvelike7@gmail.com>
2024-09-09 20:33:01 +0100

    tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        black: '#000000',
+        white: '#FFFFFF',
+        orange: '#FF885B',
+      },
+      fontFamily: {},
+      fontSize: {},
+    },
+  },
+};
