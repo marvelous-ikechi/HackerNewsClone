24db066c1e876bf970f8d4c2fbdb302899193e0b
marvelous-ikechi <marvelike7@gmail.com>
2024-09-10 21:54:15 +0100

    added splashscreen

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import AuthNavigator from './AuthNavigator';
 import {RootStackParamList} from 'src/types/navigationTypes';
 import BottomTabNavigator from './BottomTabNavigator';
+import {useSelector} from 'react-redux';
+import {RootState} from 'src/redux/store';
+import Splashscreen from 'src/components/Splashscreen';
 
 const Stack = createStackNavigator<RootStackParamList>();
 
 const RootNavigatior = () => {
+  const user = useSelector((state: RootState) => state.user);
+  const [initialRoute, setInitialRoute] = React.useState<
+    keyof RootStackParamList | undefined
+  >(undefined);
+
+  useEffect(() => {
+    if (user.email) {
+      setInitialRoute('MainStack');
+    } else {
+      setInitialRoute('AuthStack');
+    }
+  }, [user]);
+
+  if (!initialRoute) {
+    return <Splashscreen />;
+  }
+
   return (
-    <Stack.Navigator>
+    <Stack.Navigator initialRouteName={initialRoute}>
       <Stack.Screen
         options={{headerShown: false}}
         name="AuthStack"
