a3bab270ffb3a33628401506b60b075f38bd0ba5
marvelous-ikechi <marvelike7@gmail.com>
2024-09-10 21:51:52 +0100

    setup redux

--- App.tsx
+++ App.tsx
@@ -2,12 +2,19 @@ import React, {FunctionComponent} from 'react';
 import './gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import RootNavigatior from './src/navigation/RootNavigator';
+import {persistor, store} from 'src/redux/store';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
 
 const App: FunctionComponent = () => {
   return (
-    <NavigationContainer>
-      <RootNavigatior />
-    </NavigationContainer>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer>
+          <RootNavigatior />
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
   );
 };
 
