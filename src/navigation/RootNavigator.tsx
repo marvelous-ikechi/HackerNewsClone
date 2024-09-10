5180481b140963b1f4b10719703f7549c6500a21
marvelous-ikechi <marvelike7@gmail.com>
2024-09-10 11:42:56 +0100

    Bottom navigator setup

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import AuthNavigator from './AuthNavigator';
 import {RootStackParamList} from 'src/types/navigationTypes';
+import BottomTabNavigator from './BottomTabNavigator';
 
 const Stack = createStackNavigator<RootStackParamList>();
 
@@ -12,6 +13,11 @@ const RootNavigatior = () => {
         options={{headerShown: false}}
         name="AuthStack"
         component={AuthNavigator}
+      />
+      <Stack.Screen
+        options={{headerShown: false}}
+        name="MainStack"
+        component={BottomTabNavigator}
       />
     </Stack.Navigator>
   );
