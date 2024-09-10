4dab6cfc15c6deed0aeacbf33511d84682877d3f
marvelous-ikechi <marvelike7@gmail.com>
2024-09-10 15:12:38 +0100

    Signup screen completed

--- src/auth/screens/Signup.tsx
+++ src/auth/screens/Signup.tsx
@@ -1,4 +1,4 @@
-import React, {FunctionComponent} from 'react';
+import React, {FunctionComponent, useState} from 'react';
 import {Text, TextInput, TouchableOpacity, View} from 'react-native';
 import ScreenContainer from 'src/components/ScreenContainer';
 import tw from 'lib/tailwind';
@@ -6,52 +6,159 @@ import SubmitBtn from 'src/components/Button/SubmitBtn';
 import {NativeStackScreenProps} from 'react-native-screens/lib/typescript/native-stack/types';
 import {AuthStackParamList} from 'src/types/navigationTypes';
 import {CommonActions} from '@react-navigation/native';
+import useAuth from 'src/hooks/useAuth';
+import * as yup from 'yup';
+import {yupResolver} from '@hookform/resolvers/yup';
+import {Controller, useForm} from 'react-hook-form';
+import {IFormInput} from 'src/types/auth';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+import ErrorText from 'src/components/Text/ErrorText';
 
 type Props = NativeStackScreenProps<AuthStackParamList, 'Signup'>;
 
 const Signup: FunctionComponent<Props> = ({navigation}) => {
+  const {signup} = useAuth();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSignup = async (data: IFormInput) => {
+    setIsLoading(true);
+    try {
+      const response = await signup(
+        data.firstName,
+        data.lastName,
+        data.email,
+        data.password,
+      );
+      navigation.dispatch(
+        CommonActions.reset({
+          index: 0,
+          routes: [{name: 'MainStack', params: {screen: 'Home'}}],
+        }),
+      );
+      console.log('response', response);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const signUpSchema = yup.object().shape({
+    firstName: yup.string().required('First name is required'),
+    lastName: yup.string().required('Last name is required'),
+    password: yup.string().required('Password is required'),
+    email: yup.string().required('Email is required').email('Email is invalid'),
+  });
+
+  const {
+    handleSubmit,
+    control,
+    formState: {errors},
+  } = useForm({
+    defaultValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+    },
+    resolver: yupResolver(signUpSchema),
+  });
+
   return (
     <ScreenContainer>
-      <View style={tw`flex-1 w-full justify-center items-center`}>
-        <TextInput
-          placeholderTextColor={'white'}
-          placeholder="First Name"
-          style={tw`border-[0.5px] font-poppinsRegular px-3 rounded-md w-80 border-white`}
-        />
-        <TextInput
-          placeholderTextColor={'white'}
-          placeholder="Last Name"
-          style={tw`border-[0.5px] mt-4 px-3 font-poppinsRegular rounded-md w-80 border-white`}
-        />
-        <TextInput
-          placeholderTextColor={'white'}
-          placeholder="Email"
-          keyboardType="email-address"
-          style={tw`border-[0.5px] mt-4 px-3 font-poppinsRegular rounded-md w-80 border-white`}
-        />
-        <TextInput
-          placeholderTextColor={'white'}
-          placeholder="Password"
-          secureTextEntry={true}
-          style={tw`border-[0.5px] px-3 mt-4  rounded-md w-80 border-white`}
-        />
-        <SubmitBtn
-          title="Signup"
-          onPress={() =>
-            navigation.dispatch(
-              CommonActions.reset({
-                index: 0,
-                routes: [{name: 'MainStack', params: {screen: 'Home'}}],
-              }),
-            )
-          }
-        />
-        <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-          <Text style={tw`text-white font-poppinsRegular text-right mt-4`}>
-            Already have an account? Login
-          </Text>
-        </TouchableOpacity>
-      </View>
+      <KeyboardAwareScrollView
+        contentContainerStyle={tw`flex-1 w-full justify-center items-center`}>
+        <View style={tw`flex-1 w-full justify-center items-center`}>
+          <Controller
+            control={control}
+            name={'firstName'}
+            render={({field: {value, onChange, onBlur}}) => (
+              <>
+                <TextInput
+                  placeholderTextColor={'white'}
+                  placeholder="First Name"
+                  value={value}
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                  style={tw`border-[0.5px] text-white font-poppinsRegular px-3 rounded-md w-80 border-white`}
+                />
+                {errors?.firstName?.message && (
+                  <ErrorText text={errors.firstName?.message} />
+                )}
+              </>
+            )}
+          />
+          <Controller
+            control={control}
+            name={'lastName'}
+            render={({field: {value, onChange, onBlur}}) => (
+              <>
+                <TextInput
+                  placeholderTextColor={'white'}
+                  placeholder="Last Name"
+                  value={value}
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                  style={tw`border-[0.5px] text-white mt-4 px-3 font-poppinsRegular rounded-md w-80 border-white`}
+                />
+                {errors?.lastName?.message && (
+                  <ErrorText text={errors.lastName?.message} />
+                )}
+              </>
+            )}
+          />
+          <Controller
+            control={control}
+            name={'email'}
+            render={({field: {value, onChange, onBlur}}) => (
+              <>
+                <TextInput
+                  placeholderTextColor={'white'}
+                  placeholder="Email"
+                  keyboardType="email-address"
+                  value={value}
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                  style={tw`border-[0.5px] text-white mt-4 px-3 font-poppinsRegular rounded-md w-80 border-white`}
+                />
+                {errors?.email?.message && (
+                  <ErrorText text={errors.email?.message} />
+                )}
+              </>
+            )}
+          />
+          <Controller
+            control={control}
+            name={'password'}
+            render={({field: {value, onChange, onBlur}}) => (
+              <>
+                <TextInput
+                  placeholderTextColor={'white'}
+                  placeholder="Password"
+                  value={value}
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                  secureTextEntry={true}
+                  style={tw`border-[0.5px] px-3 mt-4 text-white rounded-md w-80 border-white`}
+                />
+                {errors?.password?.message && (
+                  <ErrorText text={errors.password?.message} />
+                )}
+              </>
+            )}
+          />
+          <SubmitBtn
+            title="Sign up"
+            isLoading={isLoading}
+            onPress={handleSubmit(handleSignup)}
+          />
+          <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+            <Text style={tw`text-white font-poppinsRegular text-right mt-4`}>
+              Already have an account? Login
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAwareScrollView>
     </ScreenContainer>
   );
 };
