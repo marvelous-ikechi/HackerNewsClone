21f4ae0b8f7775099ef551b76e9e218a976041c9
marvelous-ikechi <marvelike7@gmail.com>
2024-09-10 11:44:37 +0100

    Home and profile screen created

---
+++ src/Home/index.tsx
@@ -0,0 +1,13 @@
+import React, {FunctionComponent} from 'react';
+import {View} from 'react-native';
+import ScreenContainer from 'src/components/ScreenContainer';
+
+const Home: FunctionComponent = () => {
+  return (
+    <ScreenContainer>
+      <></>
+    </ScreenContainer>
+  );
+};
+
+export default Home;
---
+++ src/Profile/index.tsx
@@ -0,0 +1,12 @@
+import React, {FunctionComponent} from 'react';
+import ScreenContainer from 'src/components/ScreenContainer';
+
+const Profile: FunctionComponent = () => {
+  return (
+    <ScreenContainer>
+      <></>
+    </ScreenContainer>
+  );
+};
+
+export default Profile;
