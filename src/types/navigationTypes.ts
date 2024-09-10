99e5e3e71d19a2c870553cecd2ec22c4f22e422c
marvelous-ikechi <marvelike7@gmail.com>
2024-09-10 11:43:19 +0100

    added new stacks to navigationType

--- src/types/navigationTypes.ts
+++ src/types/navigationTypes.ts
@@ -2,9 +2,15 @@ import {NavigatorScreenParams} from '@react-navigation/native';
 
 export type RootStackParamList = {
   AuthStack: NavigatorScreenParams<AuthStackParamList>;
+  MainStack: NavigatorScreenParams<MainStackParamList>;
 };
 
 export type AuthStackParamList = {
   Login: undefined;
   Signup: undefined;
+};
+
+export type MainStackParamList = {
+  Home: undefined;
+  Profile: undefined;
 };
