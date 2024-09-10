28a0f3bfb4e6f61157c887893c3c5056c9619c65
marvelous-ikechi <marvelike7@gmail.com>
2024-09-10 21:54:53 +0100

    added name to mail to profile

--- src/Profile/index.tsx
+++ src/Profile/index.tsx
@@ -1,10 +1,20 @@
 import React, {FunctionComponent} from 'react';
+import {Text, View} from 'react-native';
+import {useSelector} from 'react-redux';
 import ScreenContainer from 'src/components/ScreenContainer';
+import tw from 'src/lib/tailwind';
+import {RootState} from 'src/redux/store';
 
 const Profile: FunctionComponent = () => {
+  const user = useSelector((state: RootState) => state.user);
   return (
     <ScreenContainer>
-      <></>
+      <View style={tw`justify-center items-center`}>
+        <Text style={tw`text-white font-poppinsMedium`}>
+          {user.firstName} {user.lastName}
+        </Text>
+        <Text style={tw`text-white font-poppinsMedium`}>{user.email}</Text>
+      </View>
     </ScreenContainer>
   );
 };
