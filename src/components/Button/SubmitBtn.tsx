6a46fefd0bce98653d8f45e7bbf6769d357b2d2d
marvelous-ikechi <marvelike7@gmail.com>
2024-09-10 15:12:55 +0100

    added loading state

--- src/components/Button/SubmitBtn.tsx
+++ src/components/Button/SubmitBtn.tsx
@@ -1,18 +1,23 @@
 import React, {FunctionComponent} from 'react';
-import {TouchableOpacity, Text} from 'react-native';
+import {TouchableOpacity, Text, ActivityIndicator} from 'react-native';
 import tw from '../../lib/tailwind';
 
 interface Props {
   title: string;
   onPress: () => void;
+  isLoading?: boolean;
 }
 
-const SubmitBtn: FunctionComponent<Props> = ({title, onPress}) => {
+const SubmitBtn: FunctionComponent<Props> = ({title, onPress, isLoading}) => {
   return (
     <TouchableOpacity
       style={tw`bg-orange mt-4 h-12 items-center w-80 rounded-md justify-center`}
       onPress={onPress}>
-      <Text style={[tw`text-white font-poppinsMedium`, {}]}>{title}</Text>
+      {isLoading ? (
+        <ActivityIndicator size="small" color="white" />
+      ) : (
+        <Text style={tw`text-white font-poppinsMedium text-base`}>{title}</Text>
+      )}
     </TouchableOpacity>
   );
 };
